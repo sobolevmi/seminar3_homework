900d0faaffbdeadc6d92341fe6cf53343ca2cb8a
Maksim Sobolev <sobmig@gmail.com>
2022-07-03 22:53:20 +0300

    закончили написание кода программы

---
+++ task19/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine ("Введите пятизначное число: ");
+int number = Convert.ToInt32 (Console.ReadLine ());
+if ((number < 10000) & (number > 99999))
+    {
+        Console.WriteLine ("Введено неверное число");
+    }
+else 
+    {
+        int [] massive ()
+        {
+            int [] array = new int [5];
+            int index = 1;
+            int num = number;
+            while (index <= array.Length)
+            {
+                array [array.Length - index] = num % 10;
+                num = num / 10;
+                index++;
+            }
+            return array;
+        }
+    
+        int [] result = massive ();
+        if (result [0] == result [4] && result [1] == result [3])
+        {
+            Console.WriteLine (number + " -> " + "да");
+        }
+        else
+        {
+            Console.WriteLine (number + " -> " + "нет");
+        }
+
+    }
